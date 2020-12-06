90b69eb209c94b972247e5a48f63b6d7e7381418
Santiago RIera <rierasanti@hotmail.com>
2020-12-05 22:27:44 -0300

    Añadiendo archivos de git

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 from scrapy.crawler import CrawlerProcess
 from scrapy.utils.project import get_project_settings
 from scrap_linkedin.spiders.spider import LinkedinSpider
+
 def ask_search()->str:
     return input("¿Que deseas buscar?: ")
+
 if __name__ == '__main__':
     process = CrawlerProcess(get_project_settings())
 
